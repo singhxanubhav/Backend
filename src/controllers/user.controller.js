1a8d73e446ee12795acefe3ea6c2da345a10bde3
Anubhav Singh <anubhavsingbkj@gmail.com>
2024-08-07 00:06:09 +0530

    All things work

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,11 @@
 import asyncHandler from "../../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import { User, user } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../cloudinary.js";
+import { ApiError } from "../../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../../utils/cloudinary.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
+
+
+
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
@@ -15,27 +18,37 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
   if (existedUser) {
     throw new ApiError(409, "User Allready exist");
   }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+ 
+
+
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-  const coverImage = await uploadOnCloudinary(coverImageLocalpath);
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
     throw new ApiError(409, "User Allready exist");
   }
 
   const user = await User.create({
-    fullname,
+    fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
     email,
     password,
-    username: username.tolowerCase(),
+    username: username.toLowerCase()
   });
 
   const createdUser = await User.findById(user._id).select(
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,5 +1,6 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";   
 import fs from "fs";
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
